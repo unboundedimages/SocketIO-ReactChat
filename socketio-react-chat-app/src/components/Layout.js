2cec81486c62a7650b499d192e1af7dce2fd5731
unboundedimages <majid.muhassan@gmail.com>
2018-02-12 06:21:14 -0500

    update: added socket to Layout.js

--- socketio-react-chat-app/src/components/Layout.js
+++ socketio-react-chat-app/src/components/Layout.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client'
 
+const socketUrl= ""
 export default class Layout extends Component {
+
+	constructor(props){
+		super(props);
+
+		this.state={
+			socket:null
+		};
+	}
+
+	initSocket = ()=>{
+		const socket = io(socketUrl)
+
+		this.setState({socket})
+	}
+
 	render() {
 		const { title } = this.props
 		return (
