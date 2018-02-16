d7ae8a69133906ec4fc64ab2ae992e2860d2d2c9
unboundedimages <majid.muhassan@gmail.com>
2018-02-16 06:42:29 -0500

    update to Factories.js include adding random userID with uuid/v4, messages will be date and time stamped, and you will be able to see that a user is currently typing a message

--- socketio-react-chat-app/src/Factories.js
+++ socketio-react-chat-app/src/Factories.js
@@ -10,13 +10,30 @@ const createUser = ({name:""} = {})=>(
 const createMessage = ({message:"", sender:""} = {}=>(
 		{
 			id:uuidv4()
-			time:new Date(Date.now()),
+			time:getTime(new Date(Date.now())),
 			message,
 			sender
 		}
 
 	)
 
+const createChat = ({messages=[], name="Community", users=[]} ={})=>(
+		{
+			id:uuidv4(),
+			name,
+			messages,
+			users,
+			typingUsers:[]
+		}
+	)
+
+
 const getTime = (date)=>{
 	return `${date.getHours()}:${("0"+date.getMinutes()).slice(-2)}`
+}
+
+module.exports={
+	createMessage,
+	createChat,
+	createUsers
 }
