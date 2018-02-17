a4a3091c8d3b440a38c52fbc62b5b4e2a8d6282c
unboundedimages <majid.muhassan@gmail.com>
2018-02-17 09:44:40 -0500

    Chat folder added in components and container for chat is started

--- socketio-react-chat-app/src/components/Layout.js
+++ socketio-react-chat-app/src/components/Layout.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import io from 'socket.io-client'
 import { USER_CONNECTED, LOGOUT } from '../Events'
 import LoginForm from './LoginForm'
+import ChatContainer from './Chats/ChatContainer'
+
 const socketUrl= "http://localhost:3231"
 export default class Layout extends Component {
 
@@ -42,10 +44,16 @@ export default class Layout extends Component {
 
 	render() {
 		const { title } = this.props
-		const { socket } = this.state
+		const { socket, user } = this.state
 		return (
 			<div className="container">
-			  <LoginForm socket={socket} setUser={this.setUser}/>
+				{
+					!user ?
+			  		<LoginForm socket={socket} setUser={this.setUser}/>
+			  		:
+			  		<ChatContainer socket={socket} user={user} logout={this.logout} />
+				}
+			
 			</div>
 		);
 	}
