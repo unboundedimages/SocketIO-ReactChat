b8683c88fd253b922ba1f1de7660bee30bb2d725
unboundedimages <majid.muhassan@gmail.com>
2018-02-16 21:23:40 -0500

    working on the socket.io server in SocketManger.js

--- socketio-react-chat-app/src/server/SocketManager.js
+++ socketio-react-chat-app/src/server/SocketManager.js
@@ -1,5 +1,39 @@
 const io = require('./index.js')
 
+const { VERIFY_USER, USER_CONNECTED, LOGOUT } = require('../Events')
+
+const { createUser, createMessage, createChat } = require('../Factories')
+
+const connectedUsers = {  }
+
 module.exports = function(socket){
 	console.log("Socket Id: " + socket.id);
+
+	socket.on(VERIFY_USER. (nickname, callback)=>{
+		if(isUser){
+			callback({ isUser:true, user:null })
+		}else{
+			callback({isUser:false, user:createUser()})
+		}
+
+
+	})
+
+
+
+}
+
+
+function addUser(userList, user){
+
+}
+
+
+function removeUser(userList, username){
+
+}
+
+
+function isUser(userList, username){
+
 }
