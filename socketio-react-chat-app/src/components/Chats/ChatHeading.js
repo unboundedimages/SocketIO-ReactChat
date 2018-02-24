4841cba19b36221ee72faa4173040f696c3e3a46
unboundedimages <majid.muhassan@gmail.com>
2018-02-24 09:31:15 -0500

    update to Chatheading.js

--- socketio-react-chat-app/src/components/Chats/ChatHeading.js
+++ socketio-react-chat-app/src/components/Chats/ChatHeading.js
@@ -1,16 +1,19 @@
 import React from 'react';
+// import FA from 'react-icons/lib/fa/video-camera'
+// import FA from 'react-icons/lib/fa/keyboard-control'
 
-export default function({name}) {
+export default function({name, numberOfUsers}) {
 	
-		return (
-			<div className="chat-header"></div>
-				<div className="user-info">
-					<div className="user-name"></div>
-					<div className="status">
-						<div className="indicator"></div>
-						<span>{numberOfUsers ? numberOfUsers : null}</span>
-					</div>
+	return (
+		<div className="chat-header">
+			<div className="user-info">
+				<div className="user-name">{name}</div>
+				<div className="status">
+					<div className="indicator"></div>
+					<span>{numberOfUsers ? numberOfUsers : null}</span>
 				</div>
-			);
+			</div>
+		</div>	
+	);
 	
 }
