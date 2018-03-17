0453a9cb584b845d66db27386a31ac258017774b
unboundedimages <majid.muhassan@gmail.com>
2018-03-17 13:49:01 -0400

    update to SideBar.js and added lodash dependency

--- socketio-react-chat-app/src/components/sidebar/SideBar.js
+++ socketio-react-chat-app/src/components/sidebar/SideBar.js
@@ -4,11 +4,39 @@ import FAMenu from 'react-icons/lib/fa/list-ul'
 import FASearch from 'react-icons/lib/fa/search'
 import MdEject from 'react-icons/lib/md/eject'
 import SideBarOption from './SideBarOption'
+import { last, get, differenceBy } from 'lodash'
+import { createChatNameFromUsers } from '../../Factories'
 
 export default class SideBar extends Component{
-		
+		static type = {
+			USERS: "users",
+			CHATS: "chats"
+		}
+		constructor(props){
+			super(props)
+			this.state = {
+				receiver:"",
+				activeSidebar: SideBar.type.CHATS
+			}
+		}
+		handleSubmit = (e) => {
+			e.preventDefault()
+			const { receiver } = this.state
+			const { onSendPrivateMessage } = this.props
+
+			onSendPrivateMessage(receiver)
+			this.setState({receiver:""})
+		}
+		addChatForUser = (receiver) => {
+			this.props.onSendPrivateMessage(receiver)
+			this.setActiveSideBar(SideBar.type.CHATS)
+		}
+		setActiveSideBar = (type) => {
+			this.setState({ activeSideBar:type })
+		}
 	render(){
-		const { chats, activeChat, user, setActiveChat, logout} = this.props
+		const { chats, activeChat, user, setActiveChat, logout, users} = this.props
+		const { receiver, activeSideBar } = this.state
 		return (
 			<div id="side-bar">
 					<div className="heading">
@@ -17,10 +45,23 @@ export default class SideBar extends Component{
 							<FAMenu />
 						</div>
 					</div>
-					<div className="search">
+
+					<form onSubmit={this.handleSubmit} className="search">
 						<i className="search-icon"><FASearch /></i>
-						<input placeholder="Search" type="text"/>
+						<input placeholder="Search" type="text" value={receiver} onChange={(e)=>{this.setState({receiver:e.target.value}) }}/>
 						<div className="plus"></div>
+					</form>
+					<div className="side-bar-select">
+						<div 
+							onClick = { ()=>{ this.setActiveSideBar(SideBar.type.CHATS) } }
+							className={`side-bar-select__option ${ activeSideBar === SideBar.type.CHATS ? 'active':''}`}>
+							<span>Chats</span>
+						</div>
+						<div 
+							onClick = { ()=>{ this.setActiveSideBar(SideBar.type.USERS) } }
+							className={`side-bar-select__option ${ activeSideBar === SideBar.type.USERS ? 'active':''}`}>
+							<span>Users</span>
+						</div>
 					</div>
 					<div 
 						className="users" 
@@ -28,35 +69,23 @@ export default class SideBar extends Component{
 						onClick={(e)=>{ (e.target === this.refs.user) && setActiveChat(null) }}>
 						
 						{
+						activeSideBar === SideBar.type.CHATS ?	
 						chats.map((chat)=>{
-							if(chat.name){
-								const lastMessage = chat.messages[chat.messages.length - 1];
-								const user = chat.users.find(({name})=>{
-									return name !== this.props.name
-								}) || { name:"Blockchain and Crytpoassets" }
-								const classNames = (activeChat && activeChat.id === chat.id) ? 'active' : ''
-								
-								return(
-								<div 
-									key={chat.id} 
-									className={`user ${classNames}`}
-									onClick={ ()=>{ setActiveChat(chat) } }
-									>
-									<div className="user-photo">{user.name[0].toUpperCase()}</div>
-									<div className="user-info">
-										<div className="name">{user.name}</div>
-										{lastMessage && <div className="last-message">{lastMessage.message}</div>}
-									</div>
-									
-								</div>
-							)
+							return(
+							<SideBarOption key = {chat.id} lastMessage = {get(last(chat.messages), 'message', '')}	name = {chat.isCommunity ? chat.name : createChatNameFromUsers(chat.users, user.name)} active = { activeChat.id === chat.id } onClick = { ()=>{ this.props.setActiveChat(chat)}} />
+								)
+							})
+							:
+								differenceBy(users, [user], 'name').map((user)=>{
+								return <SideBarOption 
+									key = { user.id }
+									name = { user.name }
+									onClick = { ()=>{ this.addChatForUser(user.name) }  }
+								/>
+							})
 							}
-
-							return null
-						})	
-						}
-						
-					</div>
+						</div>
+							
 					<div className="current-user">
 						<span>{user.name}</span>
 						<div onClick={()=>{logout()}} title="Logout" className="logout">
