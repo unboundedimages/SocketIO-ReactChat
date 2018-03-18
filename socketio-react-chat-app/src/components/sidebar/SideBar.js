4ed842b992a2e02c6a97a55d8e91ee28411495f9
unboundedimages <majid.muhassan@gmail.com>
2018-03-17 20:05:19 -0400

    update sidebar

--- socketio-react-chat-app/src/components/sidebar/SideBar.js
+++ socketio-react-chat-app/src/components/sidebar/SideBar.js
@@ -40,7 +40,7 @@ export default class SideBar extends Component{
 		return (
 			<div id="side-bar">
 					<div className="heading">
-						<div className="app-name">Our Cool Chat <FAChevronDown /></div>
+						<div className="app-name">Who's online: <FAChevronDown /></div>
 						<div className="menu">
 							<FAMenu />
 						</div>
